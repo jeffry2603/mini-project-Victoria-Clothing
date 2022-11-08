360b43a265c79d4bddd0626bce6a7da553cb0f14
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2022-11-09 00:51:10 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,79 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `<div class="footer-content">
+    <img src="img/images.jpeg" class="logo" alt="">
+
+<div class="footer-ul-container">
+    <ul class="category">
+        <li class="category-title">men</li>
+        <li><a href="#" class="footer-link">t-shirts</a></li>
+        <li><a href="#" class="footer-link">Sweatshirts</a></li>
+        <li><a href="#" class="footer-link">shirts</a></li>
+        <li><a href="#" class="footer-link">jeans</a></li>
+        <li><a href="#" class="footer-link">trousers</a></li>
+        <li><a href="#" class="footer-link">Shoes</a></li>
+        <li><a href="#" class="footer-link">casuals</a></li>
+        <li><a href="#" class="footer-link">formals</a></li>
+        <li><a href="#" class="footer-link">sports</a></li>
+        <li><a href="#" class="footer-link">watch</a></li>
+    </ul>
+    <ul class="category">
+        <li class="category-title">Women</li>
+        <li><a href="#" class="footer-link">t-shirts</a></li>
+        <li><a href="#" class="footer-link">Sweatshirts</a></li>
+        <li><a href="#" class="footer-link">shirts</a></li>
+        <li><a href="#" class="footer-link">jeans</a></li>
+        <li><a href="#" class="footer-link">trousers</a></li>
+        <li><a href="#" class="footer-link">Shoes</a></li>
+        <li><a href="#" class="footer-link">casuals</a></li>
+        <li><a href="#" class="footer-link">formals</a></li>
+        <li><a href="#" class="footer-link">sports</a></li>
+        <li><a href="#" class="footer-link">watch</a></li>
+    </ul>
+</div>
+
+</div>
+<p class="footer-title">about company</p>
+<p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet eligendi praesentium, aut corrupti odit at architecto, non modi rerum accusantium obcaecati soluta iusto in iste. Laborum non perferendis quo ducimus!
+Labore voluptatibus ipsum alias numquam iure ipsa fuga voluptate, nulla deserunt mollitia dicta deleniti aut quo quae assumenda nobis magni, iste est sed neque. Ullam nam optio vitae iure amet.</p>
+<p class="info">support emails - help@victoria.com,
+    customersupport@victoria.com</p>
+    <p class="info">Contact - 1000 2000 3000</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">terms & services</a>
+            <a href="#" class="social-link">privacy page</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+        </div>
+    </div>
+    <p class="footer-credit">VICTORIA,best apparels online store</p>
+    `;
+}
+
+
+createFooter();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
